77eda31ff77679c3a2155ddfc657f3f8238ad00f
pawlo1994 <pawlo4-94@o2.pl>
2021-05-05 16:52:10 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+{
+    const tasks = [
+        {
+            content: "obejrzeć lekcję",
+            done: false,
+        },
+        {
+            content: "zjeść obiad",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+                ${task.content}
+            </li>
+            `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    }
+    const init = () => {
+        render();
+    };
+    init();
+}
