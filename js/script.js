2b906fb2def5e36bb9c3446e101869524f6622fb
pawlo1994 <pawlo4-94@o2.pl>
2021-05-05 17:31:41 +0200

    toggle task done

--- js/script.js
+++ js/script.js
@@ -24,6 +24,11 @@
         render();
     };
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
         for (const task of tasks) {
@@ -31,6 +36,7 @@
             <li 
                 ${task.done ? " style=\"text-decoration: line-through\"" : ""} 
             >
+                <button class="js-done">Zrobione?</button>
                 <button class="js-remove">Usuń</button>
                 ${task.content}
             </li>
@@ -41,6 +47,12 @@
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
+            });
+        });
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
         });
     };
